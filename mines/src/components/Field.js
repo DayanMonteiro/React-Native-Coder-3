7b25b6c417c2d834340209f976a426c38bb650f8
DayanMonteiro <dmsmamb@gmail.com>
2022-10-19 10:56:25 -0300

    mine component

--- mines/src/components/Field.js
+++ mines/src/components/Field.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { View, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native'
 import params from '../params'
-// import Mine from './Mine'
-// import Flag from './Flag'
+import Mine from './Mine'
+
 
 export default props => {
     const { mined, opened, nearMines, exploded, flagged } = props
 
     const styleField = [styles.field]
     if (opened) styleField.push(styles.opened)
+    if (exploded) styleField.push(styles.exploded)
     if (styleField.length === 1) styleField.push(styles.regular)
 
 
@@ -22,8 +23,7 @@ export default props => {
 
     return (
         <TouchableWithoutFeedback 
-        // onPress={props.onOpen}
-            // onLongPress={props.onSelect}
+
             >
             <View style={styleField}>
                  {!mined && opened && nearMines > 0 ?
@@ -31,12 +31,9 @@ export default props => {
                         {nearMines}
                     </Text> 
                     : false }
-                {/* {mined && opened ?
+                {mined && opened ?
                     <Mine /> 
                  : false}
-                {flagged && !opened ? 
-                    <Flag /> 
-                : false}  */}
             </View>
         </TouchableWithoutFeedback>
     )
