06360cdfa0cdda79a9f68f43c4ecd0fa12672926
DayanMonteiro <dmsmamb@gmail.com>
2022-10-20 10:48:30 -0300

    starting project

---
+++ Navigation/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import TextoCentral from './components/TextoCentral'
+
+
+export default props => (
+    <SafeAreaView style={{flex: 1}}>
+        <TextoCentral>Teste</TextoCentral>
+
+    </SafeAreaView>
+)
